d3c2f4cb0866a600065475cb5830d3d8f8948bda
Ashay <ashay@flux7.com>
2017-10-28 00:11:53 +0530

    replaced array lenght calculation with integer "n" restriction in sort.c

--- sort.c
+++ sort.c
@@ -17,14 +17,16 @@ void help_opts(char *filename){
 int main(int argc, char **argv){
   int a[] = {1, 232, 4, 32, 24, 5, 6, 77, 22};
   char c;
-
+  int n = sizeof(a)/sizeof(a[0]);
+  
   if (argc != 2){
     printf("Error: Check usage\n\n");
     help_opts(argv[0]);
+    printf("\n");
     exit(1);
   }
     
-  print_array(&a[0], sizeof(a)/sizeof(a[0]));
+  print_array(&a[0], n);
   printf("\n");
   
   while ((c = getopt (argc, argv, "habc")) != -1) {
@@ -32,17 +34,17 @@ int main(int argc, char **argv){
       {
       case 'a':
         printf("Executing Bubble sort\n");
-        bubble_sort(&a[0], sizeof(a)/sizeof(a[0]));
+        bubble_sort(&a[0], n);
         break;
         
       case 'b':
         printf("Executing Selection sort\n");
-        selection_sort(&a[0], sizeof(a)/sizeof(a[0]));
+        selection_sort(&a[0], n);
         break;
 
       case 'c':
         printf("Executing Selection sort\n");
-        insertion_sort(&a[0], sizeof(a)/sizeof(a[0]));
+        insertion_sort(&a[0], n);
         break;
 
       case 'h':
@@ -57,7 +59,7 @@ int main(int argc, char **argv){
   
   printf("\n");
   printf("Sorted list follows: \n");
-  print_array(&a[0], sizeof(a)/sizeof(a[0]));
+  print_array(&a[0], n);
   printf("\n");
   return errno;
 }
